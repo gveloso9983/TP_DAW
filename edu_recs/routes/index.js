de94f5381d7abf90b78def562cff3e7bb75af6fe
Goncalo Veloso <goncalo.veloso@gen.pt>
2020-12-22 17:40:53 +0000

    minor change

--- edu_recs/routes/index.js
+++ edu_recs/routes/index.js
@@ -17,7 +17,7 @@ router.post('/registar', function(req, res) {
   var reqBody = req.body
   console.log(reqBody)
   
-  // Data retrieve
+  // Data insert
   User.registar(reqBody)
     .then(data => res.render('registar', {data:data, numero : req.body.numero, nome : req.body.nome ,curso: req.body.curso , password: req.body.password ,email : req.body.email}))
     .catch(err => res.render('error', {error: err}))
