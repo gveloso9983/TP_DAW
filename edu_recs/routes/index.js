531ed01679cc88bbb7a62c27b582da6029650128
Goncalo Veloso <goncalo.veloso@gen.pt>
2020-12-23 12:43:04 +0000

    create user page

--- edu_recs/routes/index.js
+++ edu_recs/routes/index.js
@@ -8,8 +8,19 @@ router.get('/', function(req, res) {
   res.render('index');
 });
 
+router.get('/user/:id', function(req, res) {
+  var reqId = req.params.id
+  console.log(typeof reqId)
+ 
+  // Data retrieve
+  User.lookUp(reqId)
+    .then(data => res.render('user', {user: data}, console.log(data)))
+    .catch(err => res.render('error', {error: err}))
+  ;
+});
+
 router.get('/registar', function(req, res){
-  res.render('registar')
+  res.render('register')
 })
 
 //Registar
@@ -19,7 +30,7 @@ router.post('/registar', function(req, res) {
   
   // Data insert
   User.registar(reqBody)
-    .then(data => res.render('registar', {data:data, numero : req.body.numero, nome : req.body.nome ,curso: req.body.curso , password: req.body.password ,email : req.body.email}))
+    .then(data => res.render('register', {data:data, numero : req.body.numero, nome : req.body.nome ,curso: req.body.curso , password: req.body.password ,email : req.body.email}))
     .catch(err => res.render('error', {error: err}))
   ;
 });
