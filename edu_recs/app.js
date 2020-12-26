a84c3aec7857ccc581c200189a4dcd9bc18d2234
Goncalo Veloso <goncalo.veloso@gen.pt>
2020-12-26 11:35:01 +0000

    wip

--- edu_recs/app.js
+++ edu_recs/app.js
@@ -18,10 +18,60 @@ db.once('open', function() {
   console.log("Conexão ao MongoDB realizada com sucesso...")
 });
 
+//Autenticação e Login
+
+var { v4: uuidv4 } = require('uuid');
+var session = require('express-session');
+const FileStore = require('session-file-store')(session);
+
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy
+var axios = require('axios')
+
+// Configuração da estratégia local
+passport.use(new LocalStrategy(
+  {usernameField: 'id'}, (username, password, done) => {
+    axios.get('http://localhost:7709/users/' + username)
+      .then(dados => {
+        const user = dados.data
+        if(!user) { return done(null, false, {message: 'Utilizador inexistente!\n'})}
+        if(password != user.password) { return done(null, false, {message: 'Credenciais inválidas!\n'})}
+        return done(null, user)
+      })
+      .catch(erro => done(erro))
+    })
+)
+
+// Indica-se ao passport como serializar o utilizador
+passport.serializeUser((user,done) => {
+  console.log('Serielização, id: ' + user.id)
+  done(null, user.id)
+})
+  
+// Desserialização: a partir do id obtem-se a informação do utilizador
+passport.deserializeUser((uid, done) => {
+  console.log('Desserielização, id: ' + uid)
+  axios.get('http://localhost:7709/users/' + uid)
+    .then(dados => done(null, dados.data))
+    .catch(erro => done(erro, false))
+})
+
+//
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.use(session({
+  genid: req => {
+    return uuidv4()
+  },
+  store: new FileStore(),
+  secret: 'O meu segredo',
+  resave: false,
+  saveUninitialized: false
+}))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -30,8 +80,17 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('O meu segredo'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req, res, next){
+  console.log('Signed Cookies: ', JSON.stringify(req.signedCookies))
+  console.log('Session: ', JSON.stringify(req.session))
+  next()
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
