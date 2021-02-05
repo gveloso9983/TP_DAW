f14c1c2800ffd63044bfef731b8e148fa5c1026e
Mária Medvidová <maria.medvidova@student.tuke.sk>
2021-02-05 01:50:59 +0100

    FB login

---
+++ edu_recs/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	'facebookAuth': {
+		'clientID':  '741673739820853', // your App ID
+		'clientSecret':  '0a8d3fa12ea5a05a23ffd4f6542eed06', // your App Secret
+		'callbackURL':  'http://localhost:7100/authenticate/facebook/callback'
+	}
+}
+
